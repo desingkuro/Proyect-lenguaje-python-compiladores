6c8c20f1d85992138046ccb3521d92093b143b70
Andres Licona Mahcado <100732408+desingkuro@users.noreply.github.com>
2023-03-02 09:54:27 -0500

    creacion de funcion añadir alfabetos

--- index.py
+++ index.py
@@ -1,9 +1,59 @@
 import time
+import os
+
+cantidadAlfabetos = 0
+
+class CrearListas:
+    def __init__(self):
+        self.lista =[]
+    
+    def add(self,element):
+        self.lista.append(element)
+
+    def Mostrar(self):
+        print(self.lista)
+    
+    def cantidad(self):
+        return self.lista.__len__
 
 
-def menu():
+lista = CrearListas()
+
+def añadirlistas(x,lista):
+    alfabeto = []
+    os.system('clear')
+    print("|----------------------------------------------------------------------|")
+    print("||                   *cuantos alfabetos desea añadir*                 ||")
+    print("|----------------------------------------------------------------------|")
+    cantidadAlfabetos = int(input("|-Escoja una opción:"))
+    print("|----------------------------------------------------------------------|")
+    for i in range(0,cantidadAlfabetos):
+        print("|----------------------------------------------------------------------|")
+        print(f"||       *cuantos caracteres desea añadir al alfabero {i+1}*              ||")
+        print("|----------------------------------------------------------------------|")
+        caracteres =int(input("|-Escoja una opción:"))
+        print("|----------------------------------------------------------------------|")
+        for i in range(0,caracteres):
+            element = input("Ingrese el caracter: ")
+            alfabeto.append(element)
+        lista.add(alfabeto)
+
+def mostrarAlfabeto(cantidadAlfabetos,lista):
+    os.system('clear')
+    if lista.cantidad() !=0:
+        print("\n")
+        print("|----------------------------------------------------------------------|")
+        print("||                   *Listas de alfabetos actuales*                   ||")
+        print("|----------------------------------------------------------------------|")
+        lista.Mostrar()
+    else:
+        print("|----------------------------------------------------------------------|")
+        print("||   *Nose han encontrado alfabetos, por favor ingresar alguno*       ||")
+        print("|----------------------------------------------------------------------|")
+
+def menu(lista):
     seguir=True
-
+    os.system('clear')
     while seguir:
         print("\n")
         print("|----------------------------------------------------------------------|")
@@ -25,20 +75,41 @@ def menu():
         opcion = int(input("Escoja una opción:"))
         if opcion!=12:
             if opcion < 12 and opcion >0:
-                seleccion(opcion)
+                barraDeProgreso()
+                seleccion(opcion,lista)
             else:
                 print("Escoja una opcion dentro del menu")
                 time.sleep(1.5)
         else:
             seguir=False
-            
 
-def seleccion(opcion):
+
+def barraDeProgreso():
+
+    total = 100
+    for i in range(total):
+    # Calcula el porcentaje de progreso y la cantidad de caracteres a imprimir
+        porcentaje = (i / total) * 100
+        caracteres = int(porcentaje / 2)
+    
+    # Imprime la barra de carga
+        print("[{}{}] {}%".format("=" * caracteres, " " * (50 - caracteres), int(porcentaje)), end="\r")
+    
+    # Espera un momento antes de la siguiente iteración
+        time.sleep(0.03)
+
+    print("\n")
+
+
+
+def seleccion(opcion,lista):
     x = int(opcion)
     switch={
-        1:print("caso 1")
+        1: añadirlistas(x,lista),
+        2: mostrarAlfabeto(cantidadAlfabetos,lista)
     }
         
-        
+menu(lista)
 
-menu()
+
+
