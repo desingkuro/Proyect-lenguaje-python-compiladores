a594189b90686733ff55cbb6e457021aa94746b4
Emola123 <99271785+Emola123@users.noreply.github.com>
2023-03-01 22:52:46 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import time
 
 
 def menu():
-    seguir=True
+    seguir = True
 
     while seguir:
         print("\n")
@@ -23,22 +23,54 @@ def menu():
         print("|----------------------------------------------------------------------|")
         print("\n")
         opcion = int(input("Escoja una opción:"))
-        if opcion!=12:
-            if opcion < 12 and opcion >0:
+        if opcion != 12:
+            if opcion < 12 and opcion > 0:
                 seleccion(opcion)
             else:
                 print("Escoja una opcion dentro del menu")
                 time.sleep(1.5)
         else:
-            seguir=False
-            
+            seguir = False
+
 
 def seleccion(opcion):
     x = int(opcion)
-    switch={
-        1:print("caso 1")
-    }
-        
-        
+    if(x == 1):
+        crear_listas()
+    if(x == 2):
+        mostrar_listas()
 
 menu()
+
+
+def crear_listas():
+
+    listas = []
+    nnumListas = int(input("¿Cuántas listas quieres crear? "))
+
+    for i in range(nnumListas):
+        lista = []
+        numElementos = int(input(f"¿Cuántos elementos quieres en la lista {i + 1}? "))
+
+        for j in range(numElementos):
+            elemento = input(f"Ingresa el elemento {j + 1} para la lista {i + 1}: ")
+            lista.append(elemento)
+
+        listas.append(lista)
+
+    return listas
+
+
+def mostrar_listas(listas):
+    for i, lista in enumerate(listas):
+        print(f"Lista {i + 1}: {lista}")
+
+    numLista = int(input("¿Qué lista deseas ver? Ingresa su número: "))
+    lista_elegida = listas[numLista - 1]
+
+    return lista_elegida
+
+listas_creadas = crear_listas()
+
+lista_elegida = mostrar_listas(listas_creadas)
+print(f"La lista elegida es: {lista_elegida}")
